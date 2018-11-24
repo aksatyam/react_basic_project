981807e17197b58bafb141712295e0f9fdb77c28
aksatyam <ashis@cybertrontechnologies.com>
2018-11-24 15:39:42 +0530

    app router added

--- app/index.js
+++ app/index.js
@@ -4,42 +4,14 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import React from 'react';
+import AppRoute from './routes/index';
 
-class AppMain extends Component {
+class AppMain extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-      </View>
+      <AppRoute/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
 export default AppMain;
